8f95098ae929c063208ea691044cd4108fb9b884
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-31 17:11:33 -0600

    Add disk space stats

--- src/main/java/edu/vanderbilt/yunyul/vertxtw/AdditionalOSExports.java
+++ src/main/java/edu/vanderbilt/yunyul/vertxtw/AdditionalOSExports.java
@@ -3,13 +3,18 @@ package edu.vanderbilt.yunyul.vertxtw;
 import io.prometheus.client.Collector;
 import io.prometheus.client.GaugeMetricFamily;
 
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystem;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AdditionalOSExports extends Collector {
     private static final OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+    private static final FileSystem defaultFs = Paths.get(System.getProperty("user.dir")).getFileSystem();
 
     @Override
     public List<MetricFamilySamples> collect() {
@@ -20,6 +25,23 @@ public class AdditionalOSExports extends Collector {
         mfs.add(new GaugeMetricFamily("os_avail_processors", "The number of processors available to the Java virtual machine",
                 osBean.getAvailableProcessors()));
 
+        long availDiskSpace = 0;
+        long totalDiskSpace = 0;
+
+        try {
+            for (FileStore store : defaultFs.getFileStores()) {
+                availDiskSpace += store.getUsableSpace();
+                totalDiskSpace += store.getTotalSpace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mfs.add(new GaugeMetricFamily("disk_space_bytes_used", "The disk space used in bytes for all filesystems",
+                totalDiskSpace - availDiskSpace));
+        mfs.add(new GaugeMetricFamily("disk_space_bytes_max", "The total disk space for all filesystems",
+                totalDiskSpace));
+        
         return mfs;
     }
 }
