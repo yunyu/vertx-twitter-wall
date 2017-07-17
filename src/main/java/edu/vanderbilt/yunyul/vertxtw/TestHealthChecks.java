f6d1a7af68d3007e0a26f8b9c24ab43f0a1bb751
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-16 19:49:33 -0600

    Test tooltip updating

--- src/main/java/edu/vanderbilt/yunyul/vertxtw/TestHealthChecks.java
+++ src/main/java/edu/vanderbilt/yunyul/vertxtw/TestHealthChecks.java
@@ -11,7 +11,9 @@ public class TestHealthChecks {
     public static HealthChecks produceHealthChecks(Vertx vertx) {
         HealthChecks healthChecks = HealthChecks.create(vertx);
         healthChecks.register("networking/network-available", fut -> fut.complete(Status.OK()));
-        healthChecks.register("networking/epoll-available", fut -> fut.complete(Status.KO(new JsonObject().put("level", 0.02))));
+        healthChecks.register("networking/epoll-available", fut -> fut.complete(Status.KO(
+                new JsonObject().put("level", ThreadLocalRandom.current().nextInt(100) * 0.01)
+        )));
         healthChecks.register("random-is-working", fut -> {
             if (ThreadLocalRandom.current().nextBoolean()) {
                 fut.complete(Status.OK());
